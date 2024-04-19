4d73bf1b37a740974741130b877d99d0f915ebb2
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-04-19 22:40:24 +0530

    Added testimonial

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,14 @@
 import bgElement1 from "./bg-element-1.svg"
 import bgElement2 from "./bg-element-2.svg"
-import doctorIcon from "./doctor-ico.svg"
-import patienIcon from "./patien-icon.svg"
+import doctorIcon from "./doctor-icon.svg"
+import patienIcon from "./patient-icon.svg"
 import facebookLogo from "./facebook-logo.svg";
 import instagramLogo from "./instagram-logo.svg";
 import twitterLogo from "./twitter-logo.svg";
 import logoImage from "./HeartGuardian.png";
 import closeIcon from "./close-icon.svg";
 import menuIcon from "./menu-icon.svg";
-import heartDisease from "./heart diseases.png";
+import heartDisease from "./heart diseases.jpg";
 import infected from "./infect.jpg";
 import treatments from "./treatments.jpg";
 import predictHD from "./hd pred.jpeg";
@@ -17,6 +17,9 @@ import prevent from "./prevention.jpg";
 import blog1 from "./blog1.png";
 import blog2 from "./blog2.png";
 import blog3 from "./blog3.jpg";
+import testimonial1 from "./testimonial1.jpg";
+import testimonial2 from "./testimonial2.jpg";
+import testimonial3 from "./testimonial3.jpg";
 
 export { 
     bgElement1,
@@ -37,6 +40,8 @@ export {
     prevent,
     blog1,
     blog2,
-    blog3
-
+    blog3,
+    testimonial1,
+    testimonial2,
+    testimonial3
 };
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { testimonial1, testimonial2, testimonial3 } from '../../assets'
+import './Testimonials.css'
+
+function Testimonials() {
+  return (
+    <section id="testimonials" className='dark-gray'>
+      <div className='wrapper'>
+        <h2>What is our student Say?</h2>
+      </div>
+
+      <div className='content-container'>
+        <div className='testimonial'>
+          <img src={testimonial1} alt="" />
+          <div className='reviewer-details'>
+            <div className='name'>Parakrama De Silva</div>
+            <div className='company-name'>Colombo</div>
+            <div className='review'>This is a very good website. 
+            This can easily check whether there is a risk of heart disease.</div>
+          </div>
+        </div>
+
+        <div className='testimonial'>
+          <img src={testimonial2} alt="" />
+          <div className='reviewer-details'>
+            <div className='name'>Gayani Kodikara</div>
+            <div className='company-name'>Galle</div>
+            <div className='review'>This is a website that is useful for the elderly 
+            and busy people around the country.</div>
+          </div>
+        </div>
+
+
+        <div className='testimonial'>
+          <img src={testimonial3} alt="" />
+          <div className='reviewer-details'>
+            <div className='name'>Mohomad Musthaak</div>
+            <div className='company-name'>Kandy</div>
+            <div className='review'>I used this to check my parents' heart disease risk. 
+            A website that can be recommended to everyone. </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Testimonials
