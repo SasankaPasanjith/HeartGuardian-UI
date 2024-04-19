fcf5f1c74285a605430393d4d073d825810cfa1d
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-04-19 20:43:47 +0530

    Added index JS

---
+++ src/assets/index.js
@@ -0,0 +1,36 @@
+import bgElement1 from "./bg-element-1.svg"
+import bgElement2 from "./bg-element-2.svg"
+import doctorIcon from "./doctor-ico.svg"
+import patienIcon from "./patien-icon.svg"
+import facebookLogo from "./facebook-logo.svg";
+import instagramLogo from "./instagram-logo.svg";
+import twitterLogo from "./twitter-logo.svg";
+import logoImage from "./HeartGuardian.png";
+import closeIcon from "./close-icon.svg";
+import menuIcon from "./menu-icon.svg";
+import heartDisease from "./heart diseases.png";
+import infected from "./infect.jpg";
+import treatments from "./treatments.jpg";
+import predictHD from "./hd pred.jpeg";
+import riskPopulation from "./riskpopulation.jpg";
+import prevent from "./prevention.jpg";
+
+export { 
+    bgElement1,
+    bgElement2,
+    doctorIcon,
+    patienIcon,
+    facebookLogo,
+    instagramLogo, 
+    twitterLogo,
+    logoImage,
+    closeIcon,
+    menuIcon, 
+    heartDisease,
+    infected,
+    treatments,
+    predictHD,
+    riskPopulation,
+    prevent
+
+};
