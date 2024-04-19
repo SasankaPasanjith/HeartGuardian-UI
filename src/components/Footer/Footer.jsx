87ad4787b2d1d48b66549db604a2772fe1dc0a2f
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-04-19 16:10:36 +0530

    Added Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import "./Footer.css"
+import { instagramLogo, twitterLogo, facebookLogo } from '../../assets'
+
+
+function Footer() {
+  return (
+    <footer className='black'>
+      <div className='wrapper'>
+        <div className='content-container'>
+          <div className='links'>
+            <a href='#' className='logo'>
+              Heart<span className='red'>Guardian</span>
+            </a>
+
+            <div className='social-icons'>
+              <a href='#'>
+                <img src={facebookLogo} alt=''></img>
+              </a>
+              <a href='#'>
+                <img src={twitterLogo} alt=''></img>
+              </a>
+              <a href='#'>
+                <img src={instagramLogo} alt=''></img>
+              </a>
+            </div>
+
+            <div className='copyright'>
+              This Website is designed by Sasanka Pasanjith c 2024
+            </div>
+          </div>
+
+          <div className='links'>
+            <h3>Quick Links</h3>
+            <ul>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div>
+          <div className='links'>
+            <h3>Contact Us</h3>
+            <ul>
+              <li>
+                <a href="#">sasankapasanjith@email.com</a>
+              </li>
+              <li>
+                <a href="#">0770754842</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='copyright mobile'>
+          This Website is designed by Sasanka Pasanjith c 2024
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
