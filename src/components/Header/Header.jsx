b9313e9aad7c7ba95014107c62406a76a95dcdbe
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-04-19 13:21:02 +0530

    Added Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+import Navbar from "../Navbar/Navbar"
+
+function Header() {
+  return (
+    <header>
+      <div className='wrapper'>
+        <Navbar />
+        <div className='cta'>
+          <p className='course-name'>Heart Disease Predictor</p>
+          <h1>Protecting Your Heart is our Main Duty</h1>
+          <a href='#' className='demo-btn'>
+            Predict Here
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
