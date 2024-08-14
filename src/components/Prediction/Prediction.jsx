f5797c63db45a2f225d47de768e5fb6e4b8056e9
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-08-14 13:38:12 +0530

    Fixed the user prediction results not showing bug for Login user

--- src/components/Prediction/Prediction.jsx
+++ src/components/Prediction/Prediction.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import "./Prediction.css"
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./Prediction.css";
 
 function Prediction() {
     const [formData, setFormData] = useState({
@@ -11,20 +11,20 @@ function Prediction() {
         chol: '',
         fbs: '',
         thalach: ''
-      })
+    });
 
-      const [predictionResult, setPredictionResult] = useState('')
-      const [errorMessage, setErrorMessage] = useState('')
-  
-      const handleChange = (e) => {
-          const { name, value } = e.target;
-          setFormData({ ...formData, [name]: value })
-      };
-  
-      const handleSubmit = async (e) => {
-          e.preventDefault()
-          try {
-            const token = localStorage.getItem('token'); // Retrieve the token from localStorage if available
+    const [predictionResult, setPredictionResult] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const token = localStorage.getItem('token'); // Retrieve the token from localStorage
 
             const headers = token ? { Authorization: `Bearer ${token}` } : {}; // Add token to headers if it exists
 
@@ -44,57 +44,57 @@ function Prediction() {
             setErrorMessage('Failed to connect to the prediction server.');
             setPredictionResult('');
         }
-    }
+    };
 
     return (
-      <div>
-          <section id="prediction" className='dark-gray'>
-              <form onSubmit={handleSubmit}>
-                  <label htmlFor="age">Age:</label>
-                  <input type="number" id="age" name="age" value={formData.age} onChange={handleChange} required />
+        <div>
+            <section id="prediction" className='dark-gray'>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="age">Age:</label>
+                    <input type="number" id="age" name="age" value={formData.age} onChange={handleChange} required />
 
-                  <label htmlFor="sex">Gender:</label>
-                  <select id="sex" name="sex" value={formData.sex} onChange={handleChange} required>
-                      <option value="" hidden>Select</option>
-                      <option value="0">Female</option>
-                      <option value="1">Male</option>
-                  </select>
+                    <label htmlFor="sex">Gender:</label>
+                    <select id="sex" name="sex" value={formData.sex} onChange={handleChange} required>
+                        <option value="" hidden>Select</option>
+                        <option value="0">Female</option>
+                        <option value="1">Male</option>
+                    </select>
 
-                  <label htmlFor="cp">Chest Pain Type:</label>
-                  <select id="cp" name="cp" value={formData.cp} onChange={handleChange} required>
-                      <option value="" hidden>Select</option>
-                      <option value="0">atypical angina</option>
-                      <option value="1">typical angina</option>
-                      <option value="2">asymptomatic</option>
-                      <option value="3">nonanginal pain</option>
-                  </select>
+                    <label htmlFor="cp">Chest Pain Type:</label>
+                    <select id="cp" name="cp" value={formData.cp} onChange={handleChange} required>
+                        <option value="" hidden>Select</option>
+                        <option value="0">atypical angina</option>
+                        <option value="1">typical angina</option>
+                        <option value="2">asymptomatic</option>
+                        <option value="3">nonanginal pain</option>
+                    </select>
 
-                  <label htmlFor="trestbps">Resting Blood Pressure (Between 94-200 mm Hg):</label>
-                  <input type="number" id="trestbps" name="trestbps" value={formData.trestbps} onChange={handleChange} min="94" max="200" required />
+                    <label htmlFor="trestbps">Resting Blood Pressure (Between 94-200 mm Hg):</label>
+                    <input type="number" id="trestbps" name="trestbps" value={formData.trestbps} onChange={handleChange} min="94" max="200" required />
 
-                  <label htmlFor="chol">Cholesterol (Between 126-564 mg/dl):</label>
-                  <input type="number" id="chol" name="chol" value={formData.chol} onChange={handleChange} min="126" max="564" required />
+                    <label htmlFor="chol">Cholesterol (Between 126-564 mg/dl):</label>
+                    <input type="number" id="chol" name="chol" value={formData.chol} onChange={handleChange} min="126" max="564" required />
 
-                  <label htmlFor="fbs">Fasting Blood Sugar &gt; 120 mg/dL:</label>
-                  <select id="fbs" name="fbs" value={formData.fbs} onChange={handleChange} required>
-                      <option value="" hidden>Select</option>
-                      <option value="0">No</option>
-                      <option value="1">Yes</option>
-                  </select>
+                    <label htmlFor="fbs">Fasting Blood Sugar &gt; 120 mg/dL:</label>
+                    <select id="fbs" name="fbs" value={formData.fbs} onChange={handleChange} required>
+                        <option value="" hidden>Select</option>
+                        <option value="0">No</option>
+                        <option value="1">Yes</option>
+                    </select>
 
-                  <label htmlFor="thalach">Maximum Heart Rate Achieved (Between 71 - 202):</label>
-                  <input type="number" id="thalach" name="thalach" value={formData.thalach} onChange={handleChange} min="71" max="202" required />
+                    <label htmlFor="thalach">Maximum Heart Rate Achieved (Between 71 - 202):</label>
+                    <input type="number" id="thalach" name="thalach" value={formData.thalach} onChange={handleChange} min="71" max="202" required />
 
-                  <div className="form-button-container">
-                      <button type="submit">Predict</button>
-                  </div>
-              </form>
+                    <div className="form-button-container">
+                        <button type="submit">Predict</button>
+                    </div>
+                </form>
 
-              {errorMessage && <p className="error-message">{errorMessage}</p>}
-              {predictionResult && <p className="result">{predictionResult}</p>}
-          </section>
-      </div>
-  );
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
+                {predictionResult && <p className="result">{predictionResult}</p>}
+            </section>
+        </div>
+    );
 }
 
-export default Prediction
+export default Prediction;
